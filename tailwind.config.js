a1c24bf8938cf7b9f13aca498c10db7155cc090f
chloelo <chloe.lo@ennowell.com>
2023-01-10 12:12:01 +0800

    feat: 🎸 update components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,51 @@
 module.exports = {
-  content: ["./src/**/*.{html,ejs,scss}"],
+  content: ['./src/**/*.{html,ejs,scss}'],
   theme: {
     extend: {
       colors: {
         'primary': {
+          DEFAULT: '#667080',
           900: '#3D1B7A',
           800: '#451D8F',
           700: '#5424AD',
           600: '#555',
-          500: '#667080',
+          500: '#3D1B7A',
           400: '#6584de',
           300: '#B495F3',
           200: '#C4AEF7',
           100: '#D6C7F9',
           50: '#ECE6FA',
         },
+        'secondary': {
+          DEFAULT: '#EEF1F4'
+        },
         'tahiti': {
           light: '#67e8f9',
           DEFAULT: '#06b6d4',
           dark: '#0e7490',
         },
-        'secondary': '#EEF1F4',
       },
     },
+  },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/colors/themes')['[data-theme=light]'],
+          primary: '#667080',
+          secondary: '#EEF1F4',
+          'primary-focus': '#555',
+          "secondary-content": "#667080",
+        },
+      },
+    ],
   },
   plugins: [
     require('daisyui'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
   ],
+  // daisyui: {
+
+  // }
 }
